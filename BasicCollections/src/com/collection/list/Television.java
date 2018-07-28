7c2c910dbf5709e4f33c44fb88cde911635c004a
Martal <sayali.martal@capgemini.com>
2018-07-28 11:08:06 +0530

    2807 1107

---
+++ BasicCollections/src/com/collection/list/Television.java
@@ -0,0 +1,57 @@
+package com.collection.list;
+
+public class Television {
+	String company;
+	String type;
+	boolean is3dEnabled;
+	int price;
+	
+	public Television(String company, String type, boolean is3dEnabled, int price) {
+		super();
+		this.company = company;
+		this.type = type;
+		this.is3dEnabled = is3dEnabled;
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + price;
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Television other = (Television) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (price != other.price)
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Television [company=" + company + ", type=" + type + ", is3dEnabled=" + is3dEnabled + ", price=" + price
+				+ "]";
+	}
+	
+}
